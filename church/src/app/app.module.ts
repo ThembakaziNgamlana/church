5743849b65c5d6e035e9728012ecc745ee5a1cd2
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-07-30 16:28:42 +0200

    adding code

--- church/src/app/app.module.ts
+++ church/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
+    LandingPageComponent,  
     HomePageComponent,
     SignUpComponent,
-    SignInComponent
+    SignInComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- church/tailwind.config.js
+++ church/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,js,ts}', "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-
-  ],
+  plugins:  [require("daisyui"),('flowbite/plugin')],
+  daisyui: {
+    themes: true,
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
 
